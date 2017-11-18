78070ab43ffb55ad80c71700fb884e5b6d3d602f
Hamilton Chevez <hchev001@fiu.edu>
2017-11-18 16:27:34 -0500

    Created static version of the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+class QuoteDashboard extends Component {
+  currentQuote = 'People buy thing they dont need with money they dont have to impress people they dont care';
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <QuoteContent  />
+        <Button />
+        <TweetButton />
       </div>
     );
   }
 }
 
-export default App;
+class QuoteContent extends Component {
+  render() {
+    return (
+    <div>
+      People buy things they don't need with money they don't have to impress people that don't care.
+    </div>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    return(
+      <button> New Quote </button>
+    );
+  }
+}
+
+class TweetButton extends Component {
+  render() {
+    return (
+      <button> Tweet This Quote </button>
+    );
+  }
+}
+
+
+
+export default QuoteDashboard;
